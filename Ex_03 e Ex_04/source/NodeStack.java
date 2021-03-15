209c237f0b87a5a3d9e3314fb171fa86d97559b9
grupofitgkpr <78859949+grupofitgkpr@users.noreply.github.com>
2021-03-15 16:43:13 -0300

    Exercícios de 1 a 4

---
+++ Ex_03 e Ex_04/source/NodeStack.java
@@ -0,0 +1,79 @@
+package source;
+
+public class NodeStack<E> implements Stack<E>{
+	protected Node<E> top; // referência para o nodo cabeça
+	protected int size; // quantidade de elementos na pilha
+	
+	public NodeStack() { // constrói uma pilha vazia
+		top = null;
+		size = 0;
+	}
+
+	public int size() {return size; }
+
+	public boolean isEmpty() {
+		if (top == null) return true;
+		return false;
+	}
+
+	public void push(E elem) {
+		Node<E> v = new Node<E>(elem, top); // cria e encadeia um nodo novo
+		top = v;
+		size++;
+	}
+
+	public E top() throws EmptyStackException {
+		if (isEmpty()) throw new EmptyStackException("Stack is empty.");
+		return top.getElement();
+	}
+
+	public E pop() throws EmptyStackException {
+		Node<E> aux = top;
+		
+		if (isEmpty()) throw new EmptyStackException("Stack is empty.");
+		E temp = top.getElement();
+		top = top.getNext(); // desencadeia o nodo topo
+		aux.setNext(null);
+		size--;
+		return temp;
+	}
+	
+	public String verificaExpressao(String[] c1) {
+		NodeStack<String> exp = new NodeStack<String>();
+		for (int i = 0; i <= c1.length - 1; i++) {
+			if (c1[i].equals("(") || c1[i].equals("[") || c1[i].equals("{")) {
+				exp.push(c1[i]); 
+			}
+			else if (c1[i].equals(")") || c1[i].equals("]") || c1[i].equals("}")) {
+				if (exp.isEmpty()) return "Incorreto";
+				
+				if (exp.top().equals("(") && c1[i].equals(")") || 
+					exp.top().equals("[") && c1[i].equals("]") || 
+					exp.top().equals("{") && c1[i].equals("}")) { 
+					
+					exp.pop();
+				}
+				else return "Incorreto";
+			}
+		}
+		if (exp.isEmpty())
+			return "Correto";
+		else
+			return "Incorreto";
+	}
+	
+	public String toString() {
+		String s;
+		Node<E> p = top;
+	
+		s = "[";
+		while (p != null) {
+			s += p.getElement();
+			s += ", ";
+			p = p.getNext();
+		}
+		if (s.length() > 1)
+			s = s.substring(0, s.length() - 2);
+		return s + "]";
+	}
+}
